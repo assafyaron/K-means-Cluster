ba81fd16ca93cdf803a066e7ae90f78d70f691e6
Oz C <128245316+OzCabiri@users.noreply.github.com>
2024-07-12 15:08:10 +0300

    final version of py

--- kmeans.py
+++ kmeans.py
@@ -40,10 +40,11 @@ class Vector:
 # Assert correct values for k, iter
 def assert_input(k, iter, N):
     ok = True
-    if not 1 < k < N:
+    if not ((1 < k < N) and (k % 1 == 0)):
         print("Invalid number of clusters!")
         ok = False
-    if not 1 < iter < 1000:
+        return ok
+    if not ((1 < iter < 1000) and (iter % 1 == 0)):
         print("Invalid maximum iteration!")
         ok = False
     return ok
@@ -67,45 +68,54 @@ def update_centroid_and_convergence(clusters, centroids, k):
         centroids[i] = new_centroid
     return convergence
 
-
+my_file = None
 def main():
-    # Get data from console
-    input_data = sys.argv
-    if len(input_data) < 4:
-        k, iter, filename = int(input_data[1]), 200, input_data[2]
-    else:
-        k, iter, filename = int(input_data[1]), int(input_data[2]), input_data[3]
+    try:
+        # Get data from console
+        input_data = sys.argv
+        if len(input_data) < 4:
+            k, iter, filename = float(input_data[1]), 200, input_data[2]
+        else:
+            k, iter, filename = float(input_data[1]), float(input_data[2]), input_data[3]
 
-    # Read the data from the file and add all the vectors to a list
-    vectors = []
-    my_file = open(filename, "r")
-    for line in my_file:
-        vectors.append(Vector([float(x) for x in line.replace("\n", "").split(",")]))
+        # Read the data from the file and add all the vectors to a list
+        vectors = []
+        my_file = open(filename, "r")
+        for line in my_file:
+            vectors.append(Vector([float(x) for x in line.replace("\n", "").split(",")]))
 
-    # Assert correct values for k, iter
-    if not assert_input(k, iter, len(vectors)):
-        return
+        # Assert correct values for k, iter
+        if not assert_input(k, iter, len(vectors)):
+            return
+        else:
+            k = int(k)
+            iter = int(iter)
 
-    # Initialize the centroids, convergence and the clusters
-    centroids = vectors[:k]
-    clusters = [[] for i in range(k)]
-
-    for i in range(iter):
-
-        # Clear clusters
+        # Initialize the centroids, convergence and the clusters
+        centroids = vectors[:k]
         clusters = [[] for i in range(k)]
 
-        # Assign vectors into clusters
-        for vec in vectors:
-            j = vec.find_closest_centroid(centroids)
-            clusters[j].append(vec)
+        for i in range(iter):
 
-        # Calculate new centroids from cluster and check convergence
-        if update_centroid_and_convergence(clusters, centroids, k):
-            break
+            # Clear clusters
+            clusters = [[] for i in range(k)]
 
-    # Print centroids to screen
-    print("\n".join(str(cent) for cent in centroids))
+            # Assign vectors into clusters
+            for vec in vectors:
+                j = vec.find_closest_centroid(centroids)
+                clusters[j].append(vec)
+
+            # Calculate new centroids from cluster and check convergence
+            if update_centroid_and_convergence(clusters, centroids, k):
+                break
+
+        # Print centroids to screen
+        print("\n".join(str(cent) for cent in centroids))
+
+    except:
+        print("An Error Has Ocurred") 
+    finally:
+        my_file.close()
 
 
 if __name__ == "__main__":
